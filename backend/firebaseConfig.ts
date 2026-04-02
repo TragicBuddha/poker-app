fc8acb1725f8ead43d14aa813abe376e90df4690
Hunter Cox <jamesecology96@gmail.com>
2026-04-02 15:38:15 -0400

    No message

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA3NqdyL2HSR8J2Tq8b2QVILjZmRXpefZI",
   authDomain: "poker-app-ba63f.firebaseapp.com",
